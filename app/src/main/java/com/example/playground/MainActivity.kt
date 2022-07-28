304276577dbd80d9b1f46cc5007a3b2636a9c0ee
Diego Palomar <d.palomar@cmcmarkets.com>
2022-07-28 12:29:57 +0100

    Read values from gradle.properties

--- app/src/main/java/com/example/playground/MainActivity.kt
+++ app/src/main/java/com/example/playground/MainActivity.kt
@@ -1,8 +1,5 @@
 package com.example.playground
 
-import android.content.Context
-import android.content.pm.PackageInfo
-import android.content.pm.PackageManager.NameNotFoundException
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -10,9 +7,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.playground.ui.theme.PlaygroundTheme
 
 class MainActivity : ComponentActivity() {
@@ -24,33 +19,11 @@ class MainActivity : ComponentActivity() {
         // A surface container using the 'background' color from the theme
         Surface(color = MaterialTheme.colors.background) {
           Column {
-            Greeting("Android")
-            Text(text = "Version: " + versionNumber(context))
+            Text(text = "Version code: " + BuildConfig.VERSION_CODE)
+            Text(text = "Version name: " + BuildConfig.VERSION_NAME)
           }
         }
       }
     }
-  }
-}
-
-private fun versionNumber(context: Context): String {
-  return try {
-    val pInfo: PackageInfo = context.getPackageManager().getPackageInfo(context.getPackageName(), 0)
-    pInfo.versionName
-  } catch (e: NameNotFoundException) {
-    "Unknown"
-  }
-}
-
-@Composable
-fun Greeting(name: String) {
-  Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-  PlaygroundTheme {
-    Greeting("Android")
   }
 }
